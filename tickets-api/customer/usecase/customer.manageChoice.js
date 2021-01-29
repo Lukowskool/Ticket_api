c1320096d317f01cecc2af3b7a5bf66d5d0e7900
Raggara <r.raggara@gmail.com>
2021-01-29 14:34:31 +0100

    features affichage des commandes.

--- tickets-api/customer/usecase/customer.manageChoice.js
+++ tickets-api/customer/usecase/customer.manageChoice.js
@@ -1,19 +1,48 @@
 'use strict'
 
-const CustomerController = require('../infra/controller/customer.controller');
 const TicketsController = require('../../tickets/infra/controller/tickets.controller');
 const ResponseController = require('../../response/infra/controller/response.controller')
+const Response = require('../../response/domain/response.domain')
+const ResponseCustomerManageChoiceUseCase = require('../../response/usecase/response.customerchoice');
+
+const responseType = ["undefined", "keep", "reimburse"];
 
 class CustomerManageChoiceUseCase{  
     constructor(){}
 
-    async addOrUpdateChoice(customerId, ticketId, choice){
-
+    async UpdateChoice(customerId, ticketId, choice){
+        
     }
 
-    async getChoices(){
-        //getChoices
-    } 
-
+    async getChoices(customerId){
+        const responses = await ResponseController.getCurrentByCustomer(customerId);
+        const tickets = await TicketsController.getByCustomer(customerId)
+        var result = []; 
+        if(responses){
+            responses.array.forEach(element => {
+                let ticket = tickets.find(el => el.id === element.ticketId)
+                result.push({
+                    "id" : element.id,
+                    "price" : ticket.price,
+                    "customerId" : customerId,
+                    "answer": responseType[element.choice] 
+                })
+            });
+        }
+        else{
+            tickets.array.forEach(element => {
+                tickets.array.forEach(element => {
+                    ResponseCustomerManageChoiceUseCase.initResponses(customerId, element.id)
+                    result.push({
+                        "id" : element.id,
+                        "price" : ticket.price,
+                        "customerId" : customerId,
+                        "answer": "undefined" 
+                    }) 
+                });
+            });     
+        } 
+    }
 }
+
 module.exports = new CustomerManageChoiceUseCase();
